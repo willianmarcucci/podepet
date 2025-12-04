7de50abbf2fd045b1c6004a4fa4cdd3a7461d371
Willian Marcucci <willian.marcucci@gmail.com>
2025-12-04 19:34:36 -0300

    Homepage em branco

--- app/page.tsx
+++ app/page.tsx
@@ -12,59 +12,7 @@ import {
 export default function Home() {
   return (
     <div>
-      <div className="p-6 mx-auto max-w-6xl grid gap-2">
-        <div className="flex justify-between">
-          <h2 className="font-semibold text-lg">Locais próximos</h2>
-          <Button variant={"link"}>Ver todos</Button>
-        </div>
-
-        <ItemGroup className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-        </ItemGroup>
-      </div>
-
-      <div className="p-6 mx-auto max-w-6xl grid gap-2">
-        <div className="flex justify-between">
-          <h2 className="font-semibold text-lg">Hotéis</h2>
-          <Button variant={"link"}>Ver todos</Button>
-        </div>
-
-        <ItemGroup className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-        </ItemGroup>
-      </div>
-
-      <div className="p-6 mx-auto max-w-6xl grid gap-2">
-        <div className="flex justify-between">
-          <h2 className="font-semibold text-lg">Pontos de hidratação</h2>
-          <Button variant={"link"}>Ver todos</Button>
-        </div>
-
-        <ItemGroup className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-          <PlaceItem />
-        </ItemGroup>
-      </div>
+Homepage
     </div>
   );
 }
---
+++ components/place-tag.tsx
@@ -0,0 +1,7 @@
+export default function PlaceTag() {
+    return (
+        <div className="bg-muted-foreground">
+            <span>Verificado</span>
+        </div>
+    );
+}
