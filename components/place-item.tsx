b7447a69b2f636ee2e651c67fbf9bc97acf9d963
Willian Marcucci <willian.marcucci@gmail.com>
2025-12-05 19:44:27 -0300

    Place-Item alterado

--- components/place-item.tsx
+++ components/place-item.tsx
@@ -4,14 +4,14 @@ export default function PlaceItem() {
   return (
     <div className="flex gap-4 items-center">
       {/* Imagem */}
-      <div className="h-[72px] w-[72px] bg-muted rounded-lg shrink-0"></div>
+      <div className="h-20 w-20 bg-muted rounded-lg shrink-0"></div>
 
       <div className="flex gap-4 w-full">
         {/* Informações */}
         <div className="flex flex-col w-full">
-          <p className="text-xs text-muted-foreground">0.3 km</p>
-          <p className="text-sm font-semibold">Parque da Aclimação</p>
-          <p className="text-xs text-muted-foreground">Aclimação - São Paulo</p>
+          <p className="text-sm text-muted-foreground">0.3 km</p>
+          <p className=" font-semibold">Parque da Aclimação</p>
+          <p className="text-sm text-muted-foreground">Aclimação - São Paulo</p>
         </div>
 
         {/* Avaliações */}
